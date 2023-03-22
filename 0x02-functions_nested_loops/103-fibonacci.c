b09da938abe7b9ceb1ad19a62bce47a9163ece1e
AYOUB9060 <ayoubeladib906@gmail.com>
2023-03-21 19:29:53 -0700

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,27 +2,29 @@
 
 /**
  * main - finds the sum of even-valued terms in the Fibonacci
- * Return: 0 (Success)
+ * Return: 0(Success)
  */
 
 int main(void)
 {
 	unsigned long t1 = 0, t2 = 1, sum;
-	float totalSum;
+	float total_sum = 0;
 
-	while(1)
+	while (1)
 	{
 		sum = t1 + t2;
-
-		if ( sum > 4000000)
+		if (sum > 4000000)
+		{
 			break;
+		}
 
 		if ((sum % 2) == 0)
-		       totalSum += sum;
+		{
+		     total_sum += sum;
+		}
 		t1 = t2;
-		t2 = sum;	
+		t2 = sum;
 	}
-	printf("%.0f\n", totalSum);
-
+	printf("%.0f\n", total_sum);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the first 98 Fibonacci numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	unsigned int a = 1, b = 2, c;
+
+	printf("%u, %u", a, b);
+
+	for (i = 2; i < 98; i++)
+	{
+		c = a + b;
+		printf(", %u", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
