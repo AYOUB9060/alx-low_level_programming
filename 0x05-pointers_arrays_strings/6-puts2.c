332f05f757c11d10313c84d7b97365ad73579feb
AYOUB9060 <ayoubeladib906@gmail.com>
2023-03-27 19:05:44 -0700

    a program that prints only one character out of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
